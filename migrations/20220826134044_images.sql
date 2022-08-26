80522b0afa32db4da9b12e959209a81544941f9b
Hilmar Wiegand <me@hwgnd.de>
2022-08-26 15:59:24 +0200

    Add filename to migration

--- migrations/20220826134044_images.sql
+++ migrations/20220826134044_images.sql
@@ -1,8 +1,9 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS images
 (
-    id VARCHAR(26) PRIMARY KEY NOT NULL,
-    hash VARCHAR(20) NOT NULL,
-    upvotes INTEGER DEFAULT 0,
-    downvotes INTEGER DEFAULT 0
+    id        VARCHAR(26)  PRIMARY KEY NOT NULL,
+    filename  VARCHAR(200) NOT NULL,
+    hash      VARCHAR(20)  NOT NULL,
+    upvotes   INTEGER      DEFAULT 0,
+    downvotes INTEGER      DEFAULT 0
 );
